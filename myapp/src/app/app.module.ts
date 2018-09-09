74a62046d0c5585c16902a8c1df4a205912033ee
nguyenhuutinh289 <nguyenhuutinh289995@gmail.com>
2018-09-09 13:31:59 +0700

    create component home, title,librarians

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {DataTablesModule} from 'angular-datatables'
+import {HttpClientModule} from  '@angular/common/http';
+import {RouterModule , Routes} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { TopNavMenuComponent } from './component/top-nav-menu/top-nav-menu.component';
+import { HomeComponent } from './component/home/home.component';
+import { TitleComponent } from './component/title/title.component';
+import { LibrariansComponent } from './component/librarians/librarians.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 
+const routerConfig : Routes = [
+  {path: 'home', component : HomeComponent},
+  {path: 'title', component : TitleComponent},
+  {path: 'librarians', component : LibrariansComponent},
+  {path:'',redirectTo :'/home', pathMatch : "full"},
+  { path : '**', component : PagenotfoundComponent }
+]
 
+  
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    TopNavMenuComponent
+    TopNavMenuComponent,
+    HomeComponent,
+    TitleComponent,
+    LibrariansComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    DataTablesModule
+    DataTablesModule,
+    RouterModule.forRoot(routerConfig),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
